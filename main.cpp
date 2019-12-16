307bbf6d03ede10f072fdc997d4004b14df9aa89
Matthew Hummer <mhummer@tracefirst.com>
2019-12-15 17:14:49 -0700

    add point system

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ using namespace std;
 #define RIGHT 67
 #define UP 65
 #define DOWN 66
+#define SPACE 32
 
 // Prototypes
 void play();
-void showPlayer(WINDOW * win, int char_y, int char_x);
+void resetWindow(WINDOW * win);
 
 int main(int argc, char ** argv)
 {
@@ -31,67 +32,93 @@ int main(int argc, char ** argv)
 }
 
 void play(){
-  WINDOW * win;
-  WINDOW * info_win;
-  int height, width, win_x, win_y, char_x, char_y;
+  int key;
+  int height = 40;
+  int width = 80;
+  int win_x = 5;
+  int win_y = 5;
+  int char_x = width / 2;
+  int char_y = height / 2;
+  int points = 0;
+  int coin_x = rand() % (78-1 + 1) + 1;
+  int coin_y =  rand() % (38-1 + 1) + 1;
+  int life = 3;
   string playerCoords;
   string keyPressDirection;
+  string pointStr = "POINTS: " + to_string(points) + "     ";
+  string lifeStr = "LIFE: " + to_string(life) + "     ";
+  bool playing = true;
+  WINDOW * info_win = newwin(height, width/2, win_y , win_x + width + 2);
+  WINDOW * win = newwin(height, width, win_y, win_x);
 
-  height = 40;
-  width = 80;
-  win_x = 5;
-  win_y = 5;
-  char_x = width / 2;
-  char_y = height / 2;
-  win = newwin(height, width, win_y, win_x);
-  info_win = newwin(height, width/2, win_y , win_x + width + 2);
   keypad(win, true);
   refresh();
+
   box(win, 0, 0);
-  box(info_win, 0, 0);
   mvwprintw(win, 0, 5, "[THE CURSE OF CURSES]");
+  resetWindow(win);
+  mvwprintw(win, char_y, char_x, "X");
+  mvwprintw(win, coin_y, coin_x, "$");
+
+  box(info_win, 0, 0);
   mvwprintw(info_win, 0, 5, "[INFO]");
-  showPlayer(win, char_y, char_x);
+  mvwprintw(info_win, 1, 1, lifeStr.c_str());
+  mvwprintw(info_win, 2, 1, pointStr.c_str());
+
   wrefresh(info_win);
   wrefresh(win);
 
-  while (true)
+  while (playing)
   {
-    int key;
-
+    // Interpret input
     key = getch();
-    if (key == LEFT)
+    switch (key)
     {
-      keyPressDirection = "KEYPRESS: LEFT    ";
-      char_x--;
+      case LEFT:
+        keyPressDirection = "KEYPRESS: LEFT    ";
+        char_x--;
+        break;
+      case RIGHT:
+        keyPressDirection = "KEYPRESS: RIGHT    ";
+        char_x++;
+        break;
+      case UP:
+        keyPressDirection = "KEYPRESS: UP    ";
+        char_y--;
+        break;
+      case DOWN:
+        keyPressDirection = "KEYPRESS: DOWN    ";
+        char_y++;
+        break;
+      default:
+        break;
     }
-    if (key == RIGHT)
-    {
-      keyPressDirection = "KEYPRESS: RIGHT    ";
-      char_x++;
-    }
-    if (key == UP)
-    {
-      keyPressDirection = "KEYPRESS: UP    ";
-      char_y--;
-    }
-    if (key == DOWN)
-    {
-      keyPressDirection = "KEYPRESS: DOWN    ";
-      char_y++;
-    }
-
     // Prevent out-of-bounds
-    if(char_x < 1) char_x = 1;
-    if(char_x > 78) char_x = 78;
-    if(char_y < 1) char_y = 1;
-    if(char_y > 38) char_y = 38;
+    if (char_x < 1) char_x = 1;
+    if (char_x > 78) char_x = 78;
+    if (char_y < 1) char_y = 1;
+    if (char_y > 38) char_y = 38;
+    // Check if collected the coin
+    if (char_x == coin_x &&
+        char_y == coin_y)
+    {
+      points++;
+      coin_x = rand() % (78-1 + 1) + 1;
+      coin_y =  rand() % (38-1 + 1) + 1;
+    }
     // Populate Info Window
+    lifeStr = "LIFE: " + to_string(life) + "     ";
+    pointStr = "POINTS: " + to_string(points) + "     ";
     playerCoords = "X: " + to_string(char_x) + " Y: " + to_string(char_y) + "       ";
-    mvwprintw(info_win, 1, 1, keyPressDirection.c_str());
-    mvwprintw(info_win, 2, 1, playerCoords.c_str());
-    
-    showPlayer(win, char_y, char_x);
+    mvwprintw(info_win, 1, 1, lifeStr.c_str());
+    mvwprintw(info_win, 2, 1, pointStr.c_str());
+    mvwprintw(info_win, 3, 1, keyPressDirection.c_str());
+    mvwprintw(info_win, 4, 1, playerCoords.c_str());
+
+    resetWindow(win);
+    mvwprintw(win, char_y, char_x, "X");
+    mvwprintw(win, coin_y, coin_x, "$");
+
     wrefresh(win);
     wrefresh(info_win);
   }
@@ -99,20 +126,11 @@ void play(){
   return;
 }
 
-void showPlayer(WINDOW * win, int char_y, int char_x)
+void resetWindow(WINDOW * win)
 {
-
   for (int y = 1; y < 40 - 1; y++)
-  {
     for (int x = 1; x < 80 - 1; x++)
-    {
-      string output;
-
-      output = (y == char_y && x == char_x ? "X" : " ");
-      mvwprintw(win, y, x, output.c_str());
-    }
-  }
-
+      mvwprintw(win, y, x, " ");
 
   return;
 }
